a660c61bedc0c079e886109ae61493223a092a5f
Ronald Wang <1788960+alastor1729@users.noreply.github.com>
2022-04-09 19:16:13 -0400

    PR refactoring / adding code comments && adding README File (#1)
    * Create README.md
    
    * PR refactoring

---
+++ README.md
@@ -0,0 +1,4 @@
+# python_tuples
+
+This is the Assignment from this Coursera Python class:
+https://www.coursera.org/learn/python-data/home/week/6
--- Week10_Assignment.py
+++ Week10_Assignment.py
@@ -1,23 +1,25 @@
 name = input("Enter file:")
+
 if len(name) < 1:
     name = "mbox-short.txt"
 
 fileHandler = open(name)
 
-counts = dict()
-lst = list()
 
+lstOfWords = list()
+# for comprehension for "lstOfWords: List[String]"
 for line in fileHandler:
     if not line.startswith("From "): continue
     words = line.split()
-    lst.append(words[5])
+    lstOfWords.append(words[5])
 
-lst.sort()
     
-listOfHours = list()    
-for word in lst:
-    hours = word.split(':')
-    listOfHours.append(hours[0])
+counts = dict()
+# for comprehension for counts.get() Python Dictionary function --> https://www.w3schools.com/python/ref_dictionary_get.asp
+for word in lstOfWords:
+    hour = word.split(':')
+    counts[hour[0]] = counts.get(hour[0], 0) + 1
     
-    
-print(listOfHours)
+# for comprehension for sorting the key (in ascending order)
+for key, val in sorted(counts.items()):
+    print(key, val)
